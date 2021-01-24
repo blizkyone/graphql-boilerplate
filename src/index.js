1205680070fcabb3c2d5913ca6e457e1eb273c3c
Paolo Lago <paolo@recosapp.com>
2021-01-24 12:57:35 -0600

    development SSL/HTTPS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
+import path from 'path'
+import fs from 'fs'
+import os from 'os'
 // import Subscription from './resolvers/Subscriptions.js'
 import db from './db.js'
 import Query from './resolvers/Query.js'
@@ -19,4 +22,34 @@ const server = new GraphQLServer({
       req,
    }),
 })
-server.start(() => console.log('The server is live'))
+
+let options = {
+   port: 4000,
+   endpoint: '/graphql',
+}
+
+if (server.express.get('env') === 'development') {
+   const sslKeyPath = path.join(
+      os.homedir(),
+      'Desktop/Desarrollo/ssl/localhost.key'
+   )
+   const sslCertPath = path.join(
+      os.homedir(),
+      'Desktop/Desarrollo/ssl/localhost.cert'
+   )
+
+   const https = {
+      key: fs.readFileSync(sslKeyPath),
+      cert: fs.readFileSync(sslCertPath),
+   }
+
+   options = {
+      ...options,
+      port: 4001,
+      https,
+   }
+}
+
+server.start(options, ({ port }) =>
+   console.log(`The server is live in port ${port}`)
+)
